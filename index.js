da817c2f8b685c98b4ba39c7ae6f367c923c0506
gopalbondz72344 <89642378+gopalbondz72344@users.noreply.github.com>
2022-06-10 12:17:12 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+ // This is my first js command
+ //document.getElementById("count-el").innerText = 5
+ let countEl = document.getElementById("count-el")
+ let saveEl = document.getElementById("save-el")
+ let count = 0
+ function increment()
+{
+    count += 1
+    countEl.textContent = count
+}
+function decrement()
+{
+    count -= 1
+    countEl.textContent = count
+}
+function save()
+{
+    let countstr = count + "-"
+    saveEl.textContent += countstr
+    countEl.textContent = 0
+    count = 0
+}
